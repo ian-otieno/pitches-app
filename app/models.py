163fe3bcba1c35b9c5d8ea7294d74dee838e3b11
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-07 10:30:51 +0300

    add class comments and votes db to models.py

--- app/models.py
+++ app/models.py
@@ -89,6 +89,77 @@ class Pitch(db.Model):
     comment = db.relationship("Comments", backref="pitches", lazy = "dynamic")
     vote = db.relationship("Votes", backref="pitches", lazy = "dynamic")
 
+def save_pitch(self):
+        """
+        Save the pitches 
+        """
+        db.session.add(self)
+        db.session.commit()
 
+@classmethod
+def clear_pitches(cls):
+        Pitch.all_pitches.clear()
+
+    # display pitches
+
+def get_pitches(id):
+        pitches = Pitch.query.filter_by(category_id=id).all()
+        return pitches
+
+
+
+# comments
+class Comments(db.Model):
+    """
+    User comment model for each pitch 
+    """
+
+    __tablename__ = 'comments'
+
+    # add columns
+    id = db.Column(db. Integer, primary_key=True)
+    opinion = db.Column(db.String(255))
+    time_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitches_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+
+
+    def save_comment(self):
+        """
+        Save the Comments/comments per pitch
+        """
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(self, id):
+        comment = Comments.query.order_by(Comments.time_posted.desc()).filter_by(pitches_id=id).all()
+        return comment
+
+
+
+#votes
+class Votes(db.Model):
+    """
+    class to the model votes
+    """
+    __tablename__='votes'
+
+    id = db.Column(db. Integer, primary_key=True)
+    vote = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitches_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+
+    def save_vote(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_votes(cls,user_id,pitches_id):
+        votes = Votes.query.filter_by(user_id=user_id, pitches_id=pitches_id).all()
+        return votes
+
+    def __repr__(self):
+        return f'{self.vote}:{self.user_id}:{self.pitches_id}'   
 
    
