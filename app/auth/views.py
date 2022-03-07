91c2eaa4e61decd9ad2ab23e204265be8fcbf581
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-07 11:06:02 +0300

    add login and logout function render template to views.py auth

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,3 +27,26 @@ def register():
 
 # Login function
 @auth.route('/login',methods=['GET','POST'])
+def login():
+    """
+    Function that checks whether the form is validated
+    """
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next')or url_for('main.index'))
+
+        flash(' The Username or Password is Invalid!')
+
+    title = "Pitch App|Login"
+    return render_template('auth/login.html', login_form = login_form, title = title)
+
+
+#logout function
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
