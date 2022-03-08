bb4be971d33860e7d9fb915007fd6cafaf5cec2f
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-08 14:41:29 +0300

    add imports and render templates to views.py

--- app/main/views.py
+++ app/main/views.py
@@ -41,3 +41,71 @@ def new_comment(pitch_id):
     all_comments = Comments.query.filter_by(pitch_id = pitch_id).all()
     return render_template('comment.html', comment_form = comment_form, comment = all_comments, pitch = pitch ) 
 
+main.route('/pitches/new', methods = ['POST','GET'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        category = form.category.data
+        description=form.description.data
+        user_id = current_user._get_current_object().id
+        new_pitch= Pitch(user_id =user_id,category=category,title=title, description=description)
+        new_pitch.save_pitch()
+
+        db.session.add(new_pitch)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    
+    return render_template('new_pitch.html', form = form)
+
+@main.route('/categories/<int:id>')
+def category(id):
+    category = PitchCategory.query.get(id)
+    if category is None:
+        abort(404)
+
+    pitches=Pitch.get_pitches(id)
+    return render_template('category.html', pitches=pitches, category=category)
+
+@main.route('/user/<name>')
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    user_id = current_user._get_current_object().id
+    pitches = Pitch.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+  
+    return render_template("profile/profile.html", user = user,pitches= pitches)
+
+@main.route('/user/<name>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(name):
+    user = User.query.filter_by(username =name).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',name=user.username))
+
+    return render_template('profdile/update.html',form =form)
+
+@main.route('/user/<name>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',name=name))
+
